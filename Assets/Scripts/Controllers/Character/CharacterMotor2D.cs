4d4c0aeb49634677e6ee2403fd0d75e759c209c3
george <115006794+thegeorgehewitt@users.noreply.github.com>
2024-10-26 13:00:13 +0700

    Wall Walk Cancel and Player Prefab Update
    Velocity reset when hitting wall (quicker turn around)
    Player prefab updated with checks.

--- Assets/Scripts/Controllers/Character/CharacterMotor2D.cs
+++ Assets/Scripts/Controllers/Character/CharacterMotor2D.cs
@@ -99,6 +99,8 @@ namespace Custom.Controller
             else if (!onCeiling && GetState("JumpEndedEarly")) { SetState("JumpEndedEarly", false); }
 
             onWall = wallCheck.OverlapCollider(contactFilter, contacts) > 0;
+            if (onWall && !GetState("WalkCancelled")) { SetState("WalkCancelled", true); }
+            else if (!onWall && GetState("WalkCancelled")) { SetState("WalkCancelled", false); }
         }
 
         private void FixedUpdate()
