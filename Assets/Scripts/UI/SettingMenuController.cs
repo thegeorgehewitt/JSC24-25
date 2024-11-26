255bc68eb9367a54cd68885b32cd6d11f620ee1b
L4ppy <114556422+L4ppy@users.noreply.github.com>
2024-11-27 03:05:18 +0700

    UI update for setting window

---
+++ Assets/Scripts/UI/SettingMenuController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingMenuController : MonoBehaviour
+{
+    [System.Serializable]
+    public class SettingPanel
+    {
+        public string name; // Name of the panel (e.g., "Sound", "Graphics", "Keybinding")
+        public GameObject panel; // The GameObject representing the UI panel
+    }
+
+    public SettingPanel[] settingPanels; // List of all setting panels
+
+    /// <summary>
+    /// Show the specified panel and hide others.
+    /// </summary>
+    /// <param name="panelName">Name of the panel to show</param>
+    public void ShowPanel(string panelName)
+    {
+        foreach (var settingPanel in settingPanels)
+        {
+            if (settingPanel.panel != null)
+            {
+                settingPanel.panel.SetActive(settingPanel.name.Equals(panelName));
+            }
+        }
+    }
+
+    /// <summary>
+    /// Hides all panels initially (useful for initialization).
+    /// </summary>
+    public void HideAllPanels()
+    {
+        foreach (var settingPanel in settingPanels)
+        {
+            if (settingPanel.panel != null)
+            {
+                settingPanel.panel.SetActive(false);
+            }
+        }
+    }
+
+    private void Start()
+    {
+        // Optional: Initialize with all panels hidden
+        HideAllPanels();
+    }
+}
