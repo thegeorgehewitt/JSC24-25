59bdcce72a1539d77d4b0aa1316eaa78984545de
Arden <150003683+ardenincgdp@users.noreply.github.com>
2025-03-05 06:10:56 +0700

    Simple Start Menu and Pause Menu for Demo

---
+++ Assets/Prefabs/UI/DEMO UI STUFF/Demo Start Level Behavior.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class DEMOSTARTMENU : MonoBehaviour
+{
+    [SerializeField] string SceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+            StartGame();
+        }
+    }
+    void StartGame()
+    {
+        SceneManager.LoadSceneAsync(SceneName);
+    }
+}
