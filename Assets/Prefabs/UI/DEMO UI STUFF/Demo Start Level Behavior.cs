bc9ab41e550d8df1667820c57ce216d401875fee
Arden <150003683+ardenincgdp@users.noreply.github.com>
2025-03-05 06:17:43 +0700

    Code Cleanup (I'm so dumb I'm sorry)

--- Assets/Prefabs/UI/DEMO UI STUFF/Demo Start Level Behavior.cs
+++ Assets/Prefabs/UI/DEMO UI STUFF/Demo Start Level Behavior.cs
@@ -6,13 +6,6 @@ using UnityEngine;
 public class DEMOSTARTMENU : MonoBehaviour
 {
     [SerializeField] string SceneName;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.anyKeyDown)
