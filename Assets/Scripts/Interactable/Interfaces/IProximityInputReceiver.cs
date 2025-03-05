7c579e315f4d46bc6e6c2c9c2d9e47b0226fa279
DavidHo1801004 <davidho181004@gmail.com>
2025-03-05 14:49:04 +0700

    Add: Doc for proximity interact interface.

--- Assets/Scripts/Interactable/Interfaces/IProximityInputReceiver.cs
+++ Assets/Scripts/Interactable/Interfaces/IProximityInputReceiver.cs
@@ -4,6 +4,13 @@ namespace Custom.Interactable.Interfaces
 {
     public interface IProximityInputReceiver
     {
+        /// <summary>
+        /// Called by <see cref="Custom.Controller.CharacterControlProximityInteract">character control</see>
+        /// for objects that requires interactions at close range.
+        /// </summary>
+        /// <param name="_key">     The performed <see cref="Key"/>. </param>
+        /// <param name="_phase">   If the given key was pressed this frame, returns <see cref="InputActionPhase.Started"/>. <br/>
+        ///                         If the given key was pressed this frame, returns <see cref="InputActionPhase.Canceled"/>. </param>
         public abstract void OnInputReceived(Key _key, KeyPhase _phase);
     }
 
