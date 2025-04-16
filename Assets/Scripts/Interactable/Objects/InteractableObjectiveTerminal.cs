ced51eb78cb8ce49733677cab02ecc83fb0cd44c
DavidHo1801004 <davidho181004@gmail.com>
2025-04-17 01:24:35 +0700

    Update: ObjectiveTerminal to proximity interact object.

--- Assets/Scripts/Interactable/Objects/InteractableObjectiveTerminal.cs
+++ Assets/Scripts/Interactable/Objects/InteractableObjectiveTerminal.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 using Custom.Manager.EventHandling;
-using Custom.Manager.Objective;
+using Custom.Interactable.Interfaces;
 
 namespace Custom.Interactable
 {
-    public class InteractableObjectiveTerminal : InteractableObject, IPersistent
+    public class InteractableObjectiveTerminal : MonoBehaviour, IProximityInputReceiver, IPersistent
     {
         public class DataCollectedEvent { };
         public class TerminalLoadedEvent { };
@@ -13,7 +14,9 @@ namespace Custom.Interactable
 
 
         [SerializeField] private string key;
+        [SerializeField] private SpriteRenderer spriteRenderer;
 
+        private bool dataCollected;
         private Collider2D[] colliders;
 
 
@@ -30,12 +33,23 @@ namespace Custom.Interactable
 
 
 
+        public void OnInputReceived(Key _key, KeyPhase _phase)
+        {
+            if (dataCollected) return;
+
+            if (_key == Key.E && _phase == KeyPhase.Released)
+            {
+                Interact();
+            } 
+        }
+
         public void Interact()
         {
-            if (!states.Contains("Acquired"))
+            if (!dataCollected)
             {
-                states.Add("Acquired");
-                spriteRenderer.color = new Color(0.2f, 0.2f, 0.2f);
+                dataCollected = true;
+
+                // Run data collection animation here (or just change the renderer's sprite)
             }
 
             foreach (var collider in colliders)
@@ -54,12 +68,12 @@ namespace Custom.Interactable
 
                 if (savedStateData != default(ObjectiveTerminalData))
                 {
-                    if (savedStateData.completed && !states.Contains("Acquired")) 
+                    if (savedStateData.completed && !dataCollected) 
                         Interact();
 
-                    if (!savedStateData.completed && states.Contains("Acquired"))
+                    if (!savedStateData.completed && dataCollected)
                     {
-                        states.Clear();
+                        dataCollected = false;
                         spriteRenderer.color = new Color(0.4f, 0.7f, 0.4f);
 
                         foreach (var collider in colliders)
@@ -77,11 +91,11 @@ namespace Custom.Interactable
 
             if (savedStateData != default(ObjectiveTerminalData))
             {
-                savedStateData.completed = states.Contains("Acquired");
+                savedStateData.completed = dataCollected;
             }
             else
             {
-                data.objectiveStates.Add(new ObjectiveTerminalData { Key = this.key, completed = states.Contains("Acquired") });
+                data.objectiveStates.Add(new ObjectiveTerminalData { Key = this.key, completed = dataCollected });
             }
         }
 
